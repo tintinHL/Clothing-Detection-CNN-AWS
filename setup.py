7800fec0abb112f16d1f4808c495f0bfe7ee0fc0
Hoang Long Nguyen Kim <71986602+tintinHL@users.noreply.github.com>
2026-02-14 02:42:10 +0000

    commit
    updating names and directories

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="clothing",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="Hoang Long Nguyen Kim",
+    author_email="nkhoanglong@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
